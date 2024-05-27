2a9e40c50a01d9d329a8d1781a939288bce88c2d
chornthoen <thoenchorn@gmail.com>
2024-05-27 16:28:03 +0700

    Update code

---
+++ Note-App/Folder/FolderCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FolderCollectionViewCell.swift
+//  Note-App
+//
+//  Created by Chorn Thoen on 5/25/24.
+//
+
+import UIKit
+
+class FolderCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Note-App/Folder/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  Note-App
+//
+//  Created by Chorn Thoen on 5/25/24.
+//
+
+import Foundation
